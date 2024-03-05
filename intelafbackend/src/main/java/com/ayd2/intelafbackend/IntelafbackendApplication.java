18400540b1421dc2993fc57aae5ab8cdffa56b76
froi0409 <ferrubocana@gmail.com>
2024-03-05 10:09:02 -0600

    Se creó el proyecto de spring

---
+++ intelafbackend/src/main/java/com/ayd2/intelafbackend/IntelafbackendApplication.java
@@ -0,0 +1,13 @@
+package com.ayd2.intelafbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntelafbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntelafbackendApplication.class, args);
+	}
+
+}
---
+++ intelafbackend/src/test/java/com/ayd2/intelafbackend/IntelafbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ayd2.intelafbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntelafbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
