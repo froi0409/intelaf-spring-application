9324ac44b13204e249992c1b4f4c144a36e0b61d
chejohrpp <sergioroherp@gmail.com>
2024-03-09 00:39:45 -0600

    fix the response for the customer to more easily the get response

--- intelafbackend/src/main/java/com/ayd2/intelafbackend/dto/customer/CustomerResponseDTO.java
+++ intelafbackend/src/main/java/com/ayd2/intelafbackend/dto/customer/CustomerResponseDTO.java
@@ -1,11 +1,6 @@
 package com.ayd2.intelafbackend.dto.customer;
 
 import com.ayd2.intelafbackend.entities.users.Customer;
-import com.ayd2.intelafbackend.entities.users.User;
-import jakarta.persistence.Column;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.MapsId;
-import jakarta.persistence.OneToOne;
 import lombok.Value;
 
 import java.math.BigDecimal;
@@ -13,13 +8,23 @@ import java.math.BigDecimal;
 @Value
 public class CustomerResponseDTO {
 
-     Long userIdUser;
-     BigDecimal credit;
-     User user;
+    Long userIdUser;
+    BigDecimal credit;
+    String nit;
+    String name;
+    String phone;
+    String dpi;
+    String email;
+    String address;
 
      public CustomerResponseDTO(Customer customer){
          this.userIdUser = customer.getUserIdUser();
          this.credit = customer.getCredit();
-         this.user = customer.getUser();
+         this.nit = customer.getUser().getNit();
+         this.name = customer.getUser().getName();
+         this.phone = customer.getUser().getPhone();
+         this.dpi = customer.getUser().getDpi();
+         this.email = customer.getUser().getEmail();
+         this.address = customer.getUser().getAddress();
      }
 }
