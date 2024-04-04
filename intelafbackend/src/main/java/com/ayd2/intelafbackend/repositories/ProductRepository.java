4b8261a9a004acc525e7716a3177b11c84d1a8fb
froi0409 <ferrubocana@gmail.com>
2024-04-04 14:28:15 -0600

    add query

--- intelafbackend/src/main/java/com/ayd2/intelafbackend/repositories/ProductRepository.java
+++ intelafbackend/src/main/java/com/ayd2/intelafbackend/repositories/ProductRepository.java
@@ -25,13 +25,19 @@ public interface ProductRepository extends CrudRepository<Product, String>{
     Optional<Product> findById(String id);
     
     @Query(value = "SELECT p.id_product, p.name, p.manufacturer, p.price, p.description, p.guaranty_months, SUM(shp.quantity) AS quantity\n" +
-"FROM sale s\n" +
-"JOIN sale_has_product shp ON s.id_sale = shp.sale_id_sale\n" +
-"JOIN product p ON shp.product_id_product = p.id_product\n" +
-"WHERE s.date >= :date1 AND s.date <= :date2\n" +
-"GROUP BY p.id_product, p.name\n" +
-"ORDER BY quantity DESC\n" +
-"LIMIT 10;", nativeQuery = true)
+            "FROM sale s\n" +
+            "JOIN sale_has_product shp ON s.id_sale = shp.sale_id_sale\n" +
+            "JOIN product p ON shp.product_id_product = p.id_product\n" +
+            "WHERE s.date >= :date1 AND s.date <= :date2\n" +
+            "GROUP BY p.id_product, p.name\n" +
+            "ORDER BY quantity DESC\n" +
+            "LIMIT 10;", nativeQuery = true)
     List<ProductBestSellingProjection> findBestSellingProducts( @Param("date1") LocalDate date1, @Param("date2") LocalDate date2);
+
+    @Query(value = "SELECT p.id_product, p.name, p.price " +
+            "FROM product p " +
+            "LEFT JOIN sale_has_product sp ON p.id_product = sp.product_id_product " +
+            "WHERE sp.product_id_product IS NULL", nativeQuery = true)
+    List<Object[]> findProductsNeverSold();
 
 }
